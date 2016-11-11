3fb307530d39c0b829f50152f9759c8564b35dda
2014011269 <1085315150@qq.com>
2016-11-11 15:27:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wper_smile/intentmessagetwo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.wper_smile.intentmessagetwo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn=(Button)findViewById(R.id.startBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,OtherActivity.class);
+                intent.putExtra("name","xiaoming");
+                intent.putExtra("age",20);
+                //startActivityForResult(Intent intent, int requestCode)
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==0 && resultCode==-1){
+            String str=data.getStringExtra("result");
+            Toast.makeText(this,str,Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+}
